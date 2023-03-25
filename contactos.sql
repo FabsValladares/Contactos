1d6a96d94e077f2ca78ff3132f8de611f52e7ca7
FabsValladares <99628986+FabsValladares@users.noreply.github.com>
2023-03-24 22:27:09 -0600

    contactos

--- contactos.sql
+++ contactos.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-03-2023 a las 23:21:28
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Tiempo de generación: 21-03-2023 a las 22:57:35
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,15 +32,19 @@ CREATE TABLE `contactos` (
   `nombre` varchar(100) NOT NULL,
   `telefono` varchar(15) NOT NULL,
   `latitud` varchar(200) NOT NULL,
-  `longitud` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `longitud` varchar(200) NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `contactos`
 --
 
-INSERT INTO `contactos` (`id`, `nombre`, `telefono`, `latitud`, `longitud`) VALUES
-(1, 'Fabiana Valladares', '8892-615', '15.556909', '-87.992975');
+INSERT INTO `contactos` (`id`, `nombre`, `telefono`, `latitud`, `longitud`, `imagen`) VALUES
+(12, 'Luis', '33099407', '37.421998333333335', '-122.084', '/storage/emulated/0/Android/data/com.example.ExaIIP/files/Pictures/JPEG_20230321_102321_1439503338696774514.jpg'),
+(13, 'Adolfo', '33099408', '37.421998333333335', '-122.084', '/storage/emulated/0/Android/data/com.example.ExaIIP/files/Pictures/JPEG_20230321_111319_2835513643317109282.jpg'),
+(14, 'Rodolfo', '33099408', '37.421998333333335', '-122.084', '/storage/emulated/0/Android/data/com.example.ExaIIP/files/Pictures/JPEG_20230321_111451_6972346184464667649.jpg'),
+(15, 'Alonso Rodriguez', '33099408', '37.421998333333335', '-122.084', '/storage/emulated/0/Android/data/com.example.ExaIIP/files/Pictures/JPEG_20230321_111716_6717828343246516816.jpg');
 
 --
 -- Índices para tablas volcadas
@@ -60,7 +64,7 @@ ALTER TABLE `contactos`
 -- AUTO_INCREMENT de la tabla `contactos`
 --
 ALTER TABLE `contactos`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
